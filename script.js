cd2a0dc27e894fc3af7c6ed9bc6a5abf80e0f30c
Hadi3030 <rehandaumamilhadi06@gmail.com>
2026-02-02 16:20:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ function toggleMenu() {
 function toggleDark() {
   document.body.classList.toggle("dark");
 }
+
+// ===================== ini untuk scrol slide image=============================================================
+<script>
+let index = 0;
+
+function slide(step) {
+  const slider = document.getElementById("slider");
+  const total = slider.children.length;
+
+  index += step;
+
+  if (index < 0) index = total - 1;
+  if (index >= total) index = 0;
+
+  slider.style.transform = `translateX(-${index * 100}%)`;
+}
+</script>
+
