e1b9353197e5fad6972579af41f7a52343b0fa05
Hadi3030 <rehandaumamilhadi06@gmail.com>
2026-02-11 10:58:10 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
 function toggleMenu() {
-  document.getElementById("nav-menu").classList.toggle("show");
-}
-
-function toggleDark() {
-  document.body.classList.toggle("dark");
-}
-
-// ===================== ini untuk scrol slide image=============================================================
-function toggleMenu() {
-  document.getElementById("nav-menu").classList.toggle("show");
+  document.getElementById("nav-menu").classList.toggle("active");
 }
 
 function toggleDark() {
@@ -29,4 +20,36 @@ function slide(step) {
 
   slider.style.transform = `translateX(-${index * 100}%)`;
 }
+
+// function toggleMenu() {
+//   document.getElementById("nav-menu").classList.toggle("show");
+// }
+
+// function toggleDark() {
+//   document.body.classList.toggle("dark");
+// }
+
+// // ===================== ini untuk scrol slide image=============================================================
+// function toggleMenu() {
+//   document.getElementById("nav-menu").classList.toggle("show");
+// }
+
+// function toggleDark() {
+//   document.body.classList.toggle("dark");
+// }
+
+// // ===================== SLIDER IMAGE =====================
+// let index = 0;
+
+// function slide(step) {
+//   const slider = document.getElementById("slider");
+//   const total = slider.children.length;
+
+//   index += step;
+
+//   if (index < 0) index = total - 1;
+//   if (index >= total) index = 0;
+
+//   slider.style.transform = `translateX(-${index * 100}%)`;
+// }
 
