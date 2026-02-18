190156eb198dd9580ffe7e72cd26fdf0fec8dfec
Hadi3030 <rehandaumamilhadi06@gmail.com>
2026-02-18 09:13:15 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+// function toggleMenu() {
+//   document.getElementById("nav-menu").classList.toggle("active");
+// }
+
+// function toggleDark() {
+//   document.body.classList.toggle("dark");
+// }
+
+// // ===================== SLIDER IMAGE =====================
+// let index = 0;
+
+// function slide(step) {
+//   const slider = document.getElementById("slider");
+//   const total = slider.children.length;
+
+//   index += step;
+
+//   if (index < 0) index = total - 1;
+//   if (index >= total) index = 0;
+
+//   slider.style.transform = `translateX(-${index * 100}%)`;
+// }
+
 function toggleMenu() {
   document.getElementById("nav-menu").classList.toggle("active");
 }
@@ -20,6 +43,23 @@ function slide(step) {
 
   slider.style.transform = `translateX(-${index * 100}%)`;
 }
+
+// ===================== LINKEDIN SLIDER =====================
+let linkedinIndex = 0;
+
+function slideLinkedin(step) {
+  const track = document.getElementById("linkedinTrack");
+  const cards = document.querySelectorAll(".linkedin-card");
+  const total = cards.length;
+
+  linkedinIndex += step;
+
+  if (linkedinIndex < 0) linkedinIndex = total - 1;
+  if (linkedinIndex >= total) linkedinIndex = 0;
+
+  track.style.transform = `translateX(-${linkedinIndex * 100}%)`;
+}
+
 
 // function toggleMenu() {
 //   document.getElementById("nav-menu").classList.toggle("show");
